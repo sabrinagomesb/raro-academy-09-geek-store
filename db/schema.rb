1d23d13c951f933d1d4a58450a2313f8ceb7ebda
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-05-07 23:41:00 -0300

    feat: adiciona model Product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_08_021437) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_08_023811) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,6 +38,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_08_021437) do
   create_table "customers", force: :cascade do |t|
     t.string "name", limit: 255, null: false
     t.string "cpf", limit: 11, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name", limit: 255, null: false
+    t.decimal "price", precision: 10, scale: 2, null: false
+    t.text "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
