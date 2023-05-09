392f4c6789cb84f20b079ab53f78e40c0597c26b
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-05-09 14:15:20 -0300

    feat: adiciona validacoes nos models customer e sale_product

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 class Customer < ApplicationRecord
+  include ModelsValidators
   has_many :sales, dependent: :destroy
   has_one :addresses, as: :addressable, dependent: :destroy
+
+  validates :name, :cpf, presence: true
+
+  validates :cpf, length: { is: 11 }
+  validate :name_allowed_length
 end
--- app/models/sale_product.rb
+++ app/models/sale_product.rb
@@ -3,4 +3,6 @@
 class SaleProduct < ApplicationRecord
   belongs_to :product
   belongs_to :sale
+
+  validates :amount, numericality: { only_integer: true, is_greater_than_or_equal_to: 1 }
 end
