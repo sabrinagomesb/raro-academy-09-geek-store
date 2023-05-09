0247f30c4e4c5044dc5e1f4e0a474149ef7c59df
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-05-09 15:14:58 -0300

    feat: adiciona validacao de reference e complement da tabela address

--- app/models/address.rb
+++ app/models/address.rb
@@ -12,6 +12,7 @@ class Address < ApplicationRecord
   validates :zip_code, :public_place, :number, :neighborhood, presence: true
   validates :zip_code, length: { is: 8 }
   validates :number, length: { minimim: 1, maximum: 15 }
+  validates :reference, :complement, allow_blank: true, length: { maximum: 255 }
 end
 
 # incluo validação pra bairro?
