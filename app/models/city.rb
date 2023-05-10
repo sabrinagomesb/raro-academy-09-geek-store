7eab38b6438c670a18b46375119b9f2a2a4943c8
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-05-10 02:11:30 -0300

    test: realiza testes referente ao model City

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 
 class City < ApplicationRecord
-  include ModelsValidators
+  include NameScopes
 
   belongs_to :state, inverse_of: :cities
 
   validates :name, presence: true
-  validate :name_allowed_length
+  validates :name, length: { minimum: 3, maximum: 255 }
+
+  scope :from_state_by_name, ->(state_name) { joins(:state).where(states: { name: state_name }) }
+  scope :from_state_by_acronym, ->(state_acronym) { joins(:state).where(states: { acronym: state_acronym }) }
 end
