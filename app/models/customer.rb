efc5917ee62bbb691179811dfa87d3e43a1759be
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-05-10 02:39:29 -0300

    feat: adiciona mais um scope em Customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -14,4 +14,5 @@ class Customer < ApplicationRecord
 
   scope :with_sales, -> { joins(:sales).uniq }
   scope :with_sales_by_store, ->(store_id) { joins(:sales).where(sales: { store_id: }).uniq }
+  scope :with_finished_sales, -> { joins(:sales).where(sales: { finished: true }).pluck(:name).uniq }
 end
