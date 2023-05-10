4c8902e339719f4285a0d3a2d1fce3cf9513e3e2
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-05-10 00:42:19 -0300

    feat: adiciona scopes no model product_supplier

--- app/models/product_supplier.rb
+++ app/models/product_supplier.rb
@@ -3,4 +3,7 @@
 class ProductSupplier < ApplicationRecord
   belongs_to :product
   belongs_to :supplier
+
+  scope :for_product, ->(product_id) { where(product_id: product_id) }
+  scope :for_supplier, ->(supplier_id) { where(supplier_id: supplier_id) }
 end
