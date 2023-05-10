6736e28aea8188ba573edc1d80e30332d7095665
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-05-10 04:04:27 -0300

    fix: remove funcao não utilizada de product

--- app/models/product.rb
+++ app/models/product.rb
@@ -16,14 +16,6 @@ class Product < ApplicationRecord
   validates :unit_price, numericality: { greater_than: 0 }
   validates :name, length: { minimum: 3, maximum: 255 }
 
-  before_validation :round_unit_price
-
   scope :costs_more_than, ->(amount) { where("unit_price > ?", amount.to_f) }
   scope :costs_less_than, ->(amount) { where("unit_price < ?", amount.to_f) }
-
-  private
-
-  def round_unit_price
-    self.unit_price = unit_price.round(2)
-  end
 end
