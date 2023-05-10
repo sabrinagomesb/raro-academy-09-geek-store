ef949a535e1f5e08b40893c2b1b3acca0861df53
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-05-09 23:44:52 -0300

    feat: adiciona scopes nos models state e city

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,9 +2,13 @@
 
 class City < ApplicationRecord
   include ModelsValidators
+  include NameScopes
 
   belongs_to :state, inverse_of: :cities
 
   validates :name, presence: true
   validate :name_allowed_length
+
+  scope :from_state_by_name, ->(state_name) { joins(:state).where(states: { name: state_name }) }
+  scope :from_state_by_acronym, ->(state_acronym) { joins(:state).where(states: { acronym: state_acronym }) }
 end
