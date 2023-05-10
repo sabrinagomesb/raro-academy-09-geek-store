f9c44fa65dabc1c0fe463c1bed196a8feb2e46ed
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-05-10 02:56:59 -0300

    feat: adiciona scopes nos models store e supplier

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -2,6 +2,8 @@
 
 class Supplier < ApplicationRecord
   include ModelsValidators
+  include NameScopes
+
   has_many :product_suppliers, dependent: :destroy
   has_many :products, through: :product_suppliers
   has_one :addresses, as: :addressable, dependent: :destroy
@@ -9,4 +11,6 @@ class Supplier < ApplicationRecord
   validates :name, :cnpj, presence: true
   validates :cnpj, length: { is: 14 }
   validate :name_allowed_length
+
+  scope :by_cnpj, ->(cnpj) { where(cnpj:) }
 end
