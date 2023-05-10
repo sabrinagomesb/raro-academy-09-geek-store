f71ac0013678586da91e41ae1ff7132c43d49bb1
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-05-10 17:59:03 -0300

    test: adiciona testes referente ao scope do model de Customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -11,7 +11,8 @@ class Customer < ApplicationRecord
   validates :cpf, length: { is: 11 }, uniqueness: true, numericality: { only_integer: true }
   validates :name, length: { minimum: 3, maximum: 255 }
 
-  scope :with_sales, -> { joins(:sales).uniq }
+  scope :by_cpf, ->(cpf) { where(cpf:) }
   scope :with_sales_by_store, ->(store_id) { joins(:sales).where(sales: { store_id: }).uniq }
   scope :with_finished_sales, -> { joins(:sales).where(sales: { finished: true }).pluck(:name).uniq }
+  scope :with_unfinished_sales, -> { joins(:sales).where(sales: { finished: false }).pluck(:name).uniq }
 end
