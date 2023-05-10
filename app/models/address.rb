91ab52275dd19f1b5ce4757bdc986c4142589780
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-05-10 17:58:44 -0300

    test: adiciona scopes ao model de Address

--- app/models/address.rb
+++ app/models/address.rb
@@ -13,6 +13,16 @@ class Address < ApplicationRecord
   validates :zip_code, length: { is: 8 }
   validates :number, length: { maximum: 15 }
   validates :reference, :complement, allow_blank: true, length: { maximum: 255 }
+
+  scope :by_city, ->(city) { where(city:) }
+  scope :by_state, ->(state) { from('addresses INNER JOIN cities ON addresses.city_id = cities.id').where('cities.state_id = ?', state) }
+  scope :by_zip_code, ->(zip_code) { where(zip_code: zip_code) }
+  scope :by_public_place, ->(public_place) { where(public_place: public_place) }
+  scope :by_number, ->(number) { where(number: number) }
+  scope :by_neighborhood, ->(neighborhood) { where(neighborhood: neighborhood) }
+  scope :by_reference, ->(reference) { where(reference: reference) }
+  scope :by_complement, ->(complement) { where(complement: complement) }
+  scope :by_addressable, ->(addressable) { where(addressable: addressable) }
 end
 
 # incluo validação pra bairro?
